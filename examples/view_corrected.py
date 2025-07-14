6fd7a2214d46b299eb1345354759a992d1eab98d
Olivier Saint-Vincent <olivier520100@gmail.com>
2025-07-14 17:48:45 -0400

    PyQT5 is issue on desktop

--- examples/view_corrected.py
+++ examples/view_corrected.py
@@ -1,14 +1,15 @@
 import sys
 import os
 from datetime import datetime
-
+from PIL import Image
+import numpy as np
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 from projection_io import image_importer, directory_images_importer,image_exporter
 from projection_visualization import viewer, fft_viewer, pixel_histogram_viewer,fft_pixel_histogram_viewer
-from projection_preprocessing import generate_gain_map, generate_offset_map, generate_bad_pixel_map,projection_correction
+from projection_preprocessing import generate_gain_map, generate_offset_map, generate_bad_pixel_map,projection_correction,normalize
 
 
 height,width = 2048,4096
 corrected_path = "examples/corrected_projections/"
 images = directory_images_importer(corrected_path,2048,4096)
-viewer(images)
+viewer(images[0])
