6f4026baba145c4760dd21505a89c3680c034fee
Olivier Saint-Vincent <olivier520100@gmail.com>
2025-07-14 11:02:27 -0400

    Added all preprocess functions, for some reason, the phase correlation doesn't work

--- src/projection_preprocessing.py
+++ src/projection_preprocessing.py
@@ -4,16 +4,17 @@ import napari
 import numpy as np
 import os
 import shutil
-from scipy.ndimage import zoom, convolve, label
+from scipy.ndimage import zoom, convolve, label, shift
 from scipy.fft import fft2, fftshift, ifft2, ifftshift
 from skimage.filters import window
 from os import listdir
 from os.path import isfile, join
 import math
+from skimage.registration import phase_cross_correlation
 
 
 from projection_io import image_importer, directory_images_importer
-from projection_visualization import viewer, fft_viewer
+from projection_visualization import viewer, fft_viewer, pixel_histogram_viewer,fft_pixel_histogram_viewer
 
 def gain_correction(image,gain_map,offset_map):
     corrected = (image-offset_map+ 2e-8)/(gain_map-offset_map+ 2e-8)
@@ -26,7 +27,7 @@ def bad_pixel_correction(image,bad_pixel_map,passes=2):
     current_pass = np.copy(uncorrected)
     new_bad_pixel_map = np.copy(bad_pixel_map)
 
-    while size <=9:
+    while size <=7:
 
         kernel = np.zeros([size,size])
         kernel[:,:] = 1
@@ -44,6 +45,88 @@ def bad_pixel_correction(image,bad_pixel_map,passes=2):
         size+=2
 
     return current_pass
+
+def clip_extremes(image, sigma = 3):
+
+    mean = np.mean(image)
+    std = np.std(image)
+    lower_bound = mean - sigma * std
+    upper_bound = mean + sigma * std
+    filtered = np.clip(image, lower_bound, upper_bound)
+
+    return filtered
+
+def get_maximum_coordinates(image):
+    max = image.max()
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            if image[i,j] == max:
+                return i,j, max
+
+
+def remove_streaks(image,max_iterations = 20):
+    image_list = []
+    if len(image.shape) > 2:
+        image = image[0,:,:]
+    center_mask_size = 201
+    mean_mask_size = 11
+    mean_convolve_size = 11
+
+    kernel_size = 3
+    kernel = np.zeros([kernel_size,kernel_size])
+    kernel[:,:] = 1/(kernel_size*kernel_size-1)
+    kernel[math.floor(kernel_size/2),math.floor(kernel_size/2)] = 0
+    kernel_offset = math.floor(kernel_size/2)
+
+
+    offset = math.floor(mean_mask_size/2)
+    subimage_offset = math.floor(mean_convolve_size/2)
+
+
+    fft_complex = fftshift(fft2(image))
+    peak_mask = np.copy(fft_complex)
+    center_x, center_y = peak_mask.shape
+
+    center_x = math.floor(center_x/2)
+    center_y = math.floor(center_y/2)
+
+    min_x, max_x = center_x - math.floor(center_mask_size/2),center_x+math.floor(center_mask_size/2)
+    min_y, max_y = center_y - math.floor(center_mask_size/2),center_y+math.floor(center_mask_size/2)
+
+    peak_mask[min_x:max_x,min_y:max_y] = 0
+
+    exit = False
+    counter = 0
+
+
+    while True:
+        # image_list.append(np.copy(np.log1p(np.abs(fft_complex))))
+        # image_list.append(np.copy(np.real(ifft2(ifftshift(fft_complex)))))
+        counter+=1
+        print(counter)
+        x,y,max = get_maximum_coordinates(peak_mask)
+        for i in [-1,0,1]:
+            for j in [-1,0,1]:
+                if i != 0 and j != 0:
+                    if peak_mask[x+i,y+j] == 0:
+                        exit = True
+        if exit:
+            break
+
+        peak_mask[x,y] = 0
+        mean = convolve(peak_mask[x-subimage_offset-kernel_offset:x+subimage_offset+kernel_offset,y-subimage_offset-kernel_offset:y+subimage_offset+kernel_offset],kernel)
+        fft_complex[x-subimage_offset:x+subimage_offset,y-subimage_offset:y+subimage_offset]= mean[kernel_offset:-kernel_offset,kernel_offset:-kernel_offset]
+        peak_mask[x-offset:x+offset,y-offset:y+offset] = 0
+        if counter >= max_iterations:
+            break
+
+
+    reconstructed = np.real(ifft2(ifftshift(fft_complex)))
+    # viz = np.zeros([len(image_list),fft_complex.shape[0],fft_complex.shape[1]])
+    # for i in range(len(image_list)):
+    #     viz[i,:,:] = image_list[i]
+    # viewer(viz)
+    return reconstructed
 
 def normalize(image):
     image = (image-image.min()+ 2e-8)/(image.max()-image.min()+ 2e-8)
@@ -92,29 +175,29 @@ def generate_bad_pixel_map(path, height, width, dtype=np.uint16):
     bad_pixel_map = np.round(-(normalize(bad_pixel_map))+1)
     return bad_pixel_map
 
-def projection_correction(images,gain_map,offset_map,bad_pixel_map):
+def projection_correction(images,gain_map,offset_map,bad_pixel_map,min_x,max_x,min_y,max_y):
     corrected_frames = np.zeros(images.shape)
     averaged = np.zeros(images.shape[1:])
+    kernel_size = 11
+    kernel = np.zeros([kernel_size,kernel_size])
+    kernel[:,:] = 1/(kernel_size*kernel_size)
     for i in range(images.shape[0]):
         print(i)
         frame = gain_correction(images[i,:,:],gain_map,offset_map)
         # normalized = normalize(frame)
-        corrected_frames[i,:,:]=bad_pixel_correction(frame,bad_pixel_map)
-        averaged+=corrected_frames[i,:,:]
+        bp_corrected = bad_pixel_correction(frame,bad_pixel_map)
+        removed_streaks = remove_streaks(bp_corrected)
+        corrected_frames[i,:,:] = removed_streaks
 
+    for i in range(images.shape[0]):
+        phase_difference, error, diffphase = phase_cross_correlation(corrected_frames[0,min_x:max_x,min_y:max_y], corrected_frames[i,min_x:max_x,min_y:max_y],upsample_factor=10)
+        print(phase_difference,error,diffphase)
+        corrected_frames[i, :, :] = shift(
+            corrected_frames[i, :, :], shift=(-phase_difference[0], -phase_difference[1]), order=1, mode='nearest'
+        )
 
+    for i in range(images.shape[0]):
+        averaged+=corrected_frames[i, :, :]
+
+    averaged = remove_streaks(averaged)
     return corrected_frames,averaged
-
-images = directory_images_importer("../test_images/projections/",2048,4096)
-comparison = directory_images_importer("../test_images/comparison/",2048,4096)
-gain_map = generate_gain_map("../test_images/calibration/GainMap.raw",2048,4096)
-offset_map = generate_offset_map("../test_images/calibration/OffsetMap.raw",2048,4096)
-bad_pixel_map = generate_bad_pixel_map("../test_images/calibration/BPMap.raw",2048,4096)
-
-
-
-corrected,averaged = projection_correction(images,gain_map,offset_map,bad_pixel_map)
-
-fft_viewer(corrected)
-fft_viewer(averaged)
-viewer(comparison)
--- src/projection_visualization.py
+++ src/projection_visualization.py
@@ -3,8 +3,6 @@ import numpy as np
 from scipy.fft import fft2, fftshift, ifft2, ifftshift
 import matplotlib.pyplot as plt
 
-
-
 def viewer(images):
 
     viewer, image_layer = napari.imshow(images)
@@ -15,26 +13,43 @@ def fft_viewer(images):
         base = np.abs(fftshift(fft2(images[0,:,:])))
         fft_stack = np.zeros([images.shape[0],base.shape[0],base.shape[1]])
         for i in range(images.shape[0]):
-            fft_stack[i,:,:] = np.log1p(np.abs(fftshift(fft2(images[i,:,:]))))
+            fft_stack[i,:,:] = 10* np.log1p(np.abs(fftshift(fft2(images[i,:,:]))))
         viewer, image_layer = napari.imshow(fft_stack)
         napari.run()
     else:
-
         fft_image = np.log1p(np.abs(fftshift(fft2(images))))
         viewer, image_layer = napari.imshow(fft_image)
         napari.run()
 
-# def pixel_histogram_viewer(images):
+def pixel_histogram_viewer(images):
 
-#     if len(images.shape) == 3:
-#         base = np.abs(fftshift(fft2(images[0,:,:])))
-#         fft_stack = np.zeros([images.shape[0],base.shape[0],base.shape[1]])
-#         for i in range(images.shape[0]):
-#             fft_stack[i,:,:] = np.log1p(np.abs(fftshift(fft2(images[i,:,:]))))
-#         viewer, image_layer = napari.imshow(fft_stack)
-#         napari.run()
-#     else:
+    if len(images.shape) == 3:
+        raw = images[0,:,:]
+        plt.hist(raw.ravel(), bins=512, color='gray', edgecolor='black')
+        plt.xlabel("Pixel Value")
+        plt.ylabel("Frequency")
+        plt.show()
+        plt.figure(figsize=(12, 5))
+    else:
+        raw = images[:,:]
+        plt.hist(raw.ravel(), bins=512, color='gray', edgecolor='black')
+        plt.xlabel("Pixel Value")
+        plt.ylabel("Frequency")
+        plt.show()
+        plt.figure(figsize=(12, 5))
 
-#         fft_image = np.log1p(np.abs(fftshift(fft2(images))))
-#         viewer, image_layer = napari.imshow(fft_image)
-#         napari.run()
+def fft_pixel_histogram_viewer(images):
+    if len(images.shape) == 3:
+        raw = np.log1p(np.abs(fftshift(fft2(images[0,:,:]))))
+        plt.hist(raw.ravel(), bins=512, color='gray', edgecolor='black')
+        plt.xlabel("Pixel Value")
+        plt.ylabel("Frequency")
+        plt.show()
+        plt.figure(figsize=(12, 5))
+    else:
+        raw = np.log1p(np.abs(fftshift(fft2(images[:,:]))))
+        plt.hist(raw.ravel(), bins=512, color='gray', edgecolor='black')
+        plt.xlabel("Pixel Value")
+        plt.ylabel("Frequency")
+        plt.show()
+        plt.figure(figsize=(12, 5))
